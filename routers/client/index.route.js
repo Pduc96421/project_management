d2cfea4393aa75045fc78ca80e7cb035b46a434b
Pduc96421 <pduc96421@gmail.com>
2025-03-03 23:57:13 +0700

    thêm sản phẩm vào giỏ hàng

--- routers/client/index.route.js
+++ routers/client/index.route.js
@@ -1,15 +1,21 @@
 const productRouter = require("./product.route");
 const homeRouter = require("./home.route");
 const searchRouter = require("./search.route");
+const cartRouter = require("./cart.route");
 
 const categoryMiddlerware = require("../../middlewares/client/category.middlerware");
+const cartMiddlerware = require("../../middlewares/client/cart.middlerware");
 
 module.exports = (app) => {
     app.use(categoryMiddlerware.category);
+    app.use(cartMiddlerware.cartId);
 
     app.use("/", homeRouter);
 
     app.use("/products", productRouter);
 
     app.use("/search", searchRouter);
+
+    app.use("/cart", cartRouter);
+    
 }
