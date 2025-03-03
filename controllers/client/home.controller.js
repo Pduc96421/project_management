322b98476b5c535ff755a761ce55c6671a6aa292
Pduc96421 <pduc96421@gmail.com>
2025-03-03 10:25:33 +0700

    hiện thị san phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -3,16 +3,28 @@ const productHelper = require("../../helpers/product");
 
 //[Get] /
 module.exports.index = async (req, res) => {
+    // lấy ra sản phẩm nổi bật
     const productsFeatured = await Product.find({
         featured: "1",
         deleted: false,
         status: "active",
     })
-
+    
     const newProductsFeatured = productHelper.priceNewProducts(productsFeatured);
+    //  end lấy ra sản phẩm nổi bật
 
+    // lấy ra sản phẩm mới nhất
+    const productNew = await Product.find({
+        deleted: false,
+        status: "active",
+    }).sort({ position: "desc" }).limit(6); 
+
+    const newProductNew = productHelper.priceNewProducts(productNew);
+    // end lấy ra sản phẩm mới nhất
+    
     res.render("client/pages/home/index", {
         pageTitle: "Trang chu",
         productsFeatured: newProductsFeatured,
+        productNew: newProductNew,
     });
 }
