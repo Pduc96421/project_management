21659d59e3df2d440af56265db0133f99c4b9522
Pduc96421 <pduc96421@gmail.com>
2025-03-05 05:11:11 +0700

    làm trang đặt hàng thành công

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -34,3 +34,74 @@ module.exports.index = async (req, res) => {
         cartDetail: cart,
     });
 };
+
+// [POST] /checkout/order
+module.exports.order = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const userInfo = req.body;
+
+    const cart = await Cart.findOne({
+        _id: cartId,
+    });
+
+    let products = [];
+    for (const product of cart.products) {
+        const productInfo = await Product.findOne({
+            _id: product.product_id,
+        });
+
+        const objectProduct = {
+            product_id: product.product_id,
+            price: productInfo.price,
+            discountPercentage: productInfo.discountPercentage,
+            quantity: product.quantity,
+        }
+
+        products.push(objectProduct);
+    }
+
+    const objectOrder = {
+        cart_id: cartId,
+        userInfo: userInfo,
+        products: products,
+    };
+
+    const order = new Order(objectOrder);
+    await order.save();
+
+    await Cart.updateOne({
+        _id: cartId,
+    }, {
+        products: [],
+    });
+
+    res.redirect(`/checkout/success/${order.id}`);
+};
+
+// [Get] /checkout/
+module.exports.success = async (req, res) => {
+    const order = await Order.findOne({
+        _id: req.params.orderId,
+    });
+
+    for(const product of order.products){
+        const productInfo = await Product.findOne({
+            _id: product.product_id,
+        }).select("title thumbnail");
+
+        product.productInfo = productInfo;
+
+        product.priceNew = productHelper.priceNewProduct(product);
+
+        product.totalPrice = product.priceNew * product.quantity;
+    }
+
+    order.totalPrice = order.products.reduce((sum, item) => {
+        return sum + item.totalPrice;
+    }, 0);
+
+    res.render("client/pages/checkout/success", {
+        pageTitle: "Đặt hàng thành công",
+        order: order,
+    });
+};
