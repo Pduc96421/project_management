fc1b65fa69848f86f982f5bc50079106c7faad9a
Pduc96421 <pduc96421@gmail.com>
2025-03-05 06:13:24 +0700

    đăng nhập 2

--- routers/client/index.route.js
+++ routers/client/index.route.js
@@ -7,10 +7,12 @@ const userRouter = require("./user.route");
 
 const categoryMiddlerware = require("../../middlewares/client/category.middlerware");
 const cartMiddlerware = require("../../middlewares/client/cart.middlerware");
+const userMiddlerware = require("../../middlewares/client/user.middlerware");
 
 module.exports = (app) => {
     app.use(categoryMiddlerware.category);
     app.use(cartMiddlerware.cartId);
+    app.use(userMiddlerware.infoUser);
 
     app.use("/", homeRouter);
 
