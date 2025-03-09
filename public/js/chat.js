efc2fb66cbf20a136bfc5edb866d2a0a6cbdebd2
Pduc96421 <pduc96421@gmail.com>
2025-03-09 20:37:57 +0700

    làm tính năng chat cơ bản

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,15 @@
-var socket = io();
+
+// CLIENT_SEND_MESSAGE
+const formSentData = document.querySelector(".chat .inner-form");
+if(formSentData){
+    formSentData.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const content = e.target.elements.content.value;
+        if(content){
+            socket.emit("CLIENT_SEND_MESSAGE", content);
+            e.target.elements.content.value = "";
+        }
+    });
+}
+//END CLIENT_SEND_MESSAGE
+
