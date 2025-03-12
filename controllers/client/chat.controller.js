07fdf975a43fb2d0314691e6539b0bb680a67f99
Pduc96421 <pduc96421@gmail.com>
2025-03-12 09:23:04 +0700

    hiên tên room chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,11 +1,14 @@
 const Chat = require("../../models/chat.model");
 const User = require("../../models/user.model");
+const RoomChat = require("../../models/room-chat.model");
 
 const chatSocket = require("../../sockets/client/chat.socket");
 
 // [GET] /chat/:roomChatId
 module.exports.index = async (req, res) => {
     const roomChatId = req.params.roomChatId;
+    const userId = res.locals.user.id;
+    let nameRoom = "";
 
     // SocketIo
     chatSocket(req, res);
@@ -21,12 +24,29 @@ module.exports.index = async (req, res) => {
         const infoUser = await User.findOne({
             _id: chat.user_id,
         }).select("fullName");
-
         chat.infoUser = infoUser;
+    }
+
+    const roomChat = await RoomChat.findOne({
+        _id: roomChatId,
+        deleted: false,
+    });
+
+    const UserB = roomChat.users.find(user => user.user_id != userId);
+
+    if (roomChat.typeRoom == "group") {
+        nameRoom = roomChat.title;
+    } else if (roomChat.typeRoom == "friend") {
+        const infoUserB = await User.findOne({
+            _id: UserB.user_id,
+        }).select("fullName avatar id");
+
+        nameRoom = infoUserB.fullName;
     }
 
     res.render("client/pages/chat/index", {
         pageTitle: "Chat",
         chats: chats,
+        nameRoom: nameRoom,
     });
 };
