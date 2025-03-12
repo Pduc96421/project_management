1a48da56725f81425349fe3c78d4de532d534e15
Pduc96421 <pduc96421@gmail.com>
2025-03-12 08:52:00 +0700

    hiển thị danh sách phòng chat

--- controllers/client/rooms-chat.controller.js
+++ controllers/client/rooms-chat.controller.js
@@ -3,13 +3,17 @@ const User = require("../../models/user.model");
 
 //[Get] /rooms-chat/
 module.exports.index = async (req, res) => {
-    const room = await RoomChat.find({
+    const userId = res.locals.user.id;
+
+    const listRoomChat = await RoomChat.find({
         deleted: false,
+        typeRoom: "group",
+        "users.user_id": userId,
     });
 
     res.render("client/pages/rooms-chat/index", {
         pageTitle: "Danh sách phòng",
-        room: room,
+        listRoomChat: listRoomChat,
     });
 }
 
@@ -36,26 +40,31 @@ module.exports.createPost = async (req, res) => {
     const title = req.body.title;
     const usersId = req.body.usersId;
 
-    const dataChat = {
-        title: title,
-        typeRoom: "group",
-        users: [],
-    };
+    if (typeof(usersId) == "object") {
+        const dataChat = {
+            title: title,
+            typeRoom: "group",
+            users: [],
+        };
 
-    usersId.forEach(userId => {
-        dataChat.users.push({
-            user_id: userId,
-            role: "user",
+        usersId.forEach(userId => {
+            dataChat.users.push({
+                user_id: userId,
+                role: "user",
+            });
         });
-    });
 
-    dataChat.users.push({
-        user_id: res.locals.user.id,
-        role: "superAdmin",
-    });
+        dataChat.users.push({
+            user_id: res.locals.user.id,
+            role: "superAdmin",
+        });
 
-    const room = new RoomChat(dataChat);
-    await room.save();
+        const room = new RoomChat(dataChat);
+        await room.save();
 
-    res.redirect(`/chat/${room.id}`);
+        res.redirect(`/chat/${room.id}`);
+    } else {
+        req.flash("error", "chọn đủ 2 thành viên trong nhóm");
+        res.redirect("back");
+    }
 }
--- routers/client/rooms-chat.route.js
+++ routers/client/rooms-chat.route.js
@@ -3,6 +3,8 @@ const router = express.Router();
 
 const controller = require("../../controllers/client/rooms-chat.controller");
 
+const chatMiddlerware = require("../../middlewares/client/chat.middlerware");
+
 router.get("/", controller.index);
 
 router.get("/create", controller.create);
